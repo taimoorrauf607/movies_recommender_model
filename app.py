a66e19bb62b215e5ddaa035be1f5f8f9aa1aa6d1
temii <taimoorrauf607@gmail.com>
2025-01-28 15:43:51 -0800

    poster remove for better performance

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open('similarity.pkl','rb'))
 
 # fetch poster using TMDB Api
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+# def fetch_poster(movie_id):
+#     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+#     data = requests.get(url)
+#     data = data.json()
+#     poster_path = data['poster_path']
+#     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+#     return full_path
 
 # main recommend function
 
@@ -28,15 +28,15 @@ def recommend(movie):
     movie_list = sorted(list(enumerate(distance)),reverse=True,key=lambda x:x[1])[1:6]
 
     recommend_movie = []
-    poster = []
+    # poster = []
     for i in movie_list:
 
-        movie_id = movies.iloc[i[0]].id # movie_id is column
-        # fetch poster from api
-        poster.append(fetch_poster(movie_id))
+        # movie_id = movies.iloc[i[0]].id # movie_id is column
+        # # fetch poster from api
+        # poster.append(fetch_poster(movie_id))
 
         recommend_movie.append(movies.iloc[i[0]].title)  # title is column
-    return recommend_movie , poster
+    return recommend_movie # ,poster
     
 
 st.title('Movies recommendation System')  # title on top
@@ -48,21 +48,21 @@ selected_movie = st.selectbox (
 )
 
 if st.button("Recommend"):
-    recommend_movie, poster = recommend(selected_movie)
+    recommend_movie= recommend(selected_movie)   #,poster
     
     col1 , col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommend_movie[0])
-        st.image(poster[0])
+        # st.image(poster[0])
     with col2:
         st.text(recommend_movie[1])
-        st.image(poster[1])
+        # st.image(poster[1])
     with col3:
         st.text(recommend_movie[2])
-        st.image(poster[2])
+        # st.image(poster[2])
     with col4:
         st.text(recommend_movie[3])
-        st.image(poster[3])
+        # st.image(poster[3])
     with col5:
         st.text(recommend_movie[4])
-        st.image(poster[4])
+        # st.image(poster[4])
